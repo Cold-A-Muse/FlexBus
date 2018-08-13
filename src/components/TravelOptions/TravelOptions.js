84181ce846901336011171b12e1eec2fd75ad6d8
Jonathan Mitnik <JMitnik@gmail.com>
2018-08-13 19:34:15 +0200

    Travel Options fetch

--- src/components/TravelOptions/TravelOptions.js
+++ src/components/TravelOptions/TravelOptions.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import './TravelOptions.css';
+import sampleResponseBody from '../../data/KLM-API/sample-response-body.json';
 
 class TravelOptions extends Component {
+    KLMOfferURL = "https://api.klm.com/opendata/flightoffers/available-offers";
+
+    state = {
+        currentOffers: []
+    };
+
+    componentDidMount() {
+        this.fetchAvailableOffers();
+    }
+
+    fetchAvailableOffers = () => {
+        fetch(this.KLMOfferURL, {
+            method: "POST",
+            headers: {
+             "AFKL-TRAVEL-Country": "NL",
+             "AFKL-TRAVEL-Host": "KL",
+             "api-Key": "dbcprwdc6vmjmgmw46rf2rnt",
+             "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(sampleResponseBody)
+        })
+        .then(response => response.json())
+        .then(data => this.setState({
+            currentOffers: data.flightProducts
+        }));
+    }
+
     render() {
         return (
             <div className="travel-options">
